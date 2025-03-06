491ec76826e87a725c5acd4aad37192559f82275
unknown <dhakshesh.sam@gmail.com>
2025-03-06 22:48:37 +0100

    DateChecker and Disaplay

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from datetime import date
-f = open("DailyNotes.txt", "a")
+def dateSearch(file,dateString):
+    for line in open(file, "r"):
+        if dateString in line:
+            return True
+
+fileName = "DailyNotes.txt"
+fWriter = open(fileName, "a")
 userNote = input("What would you like to note down today?\n");
-f.write(str(date.today())+"\n")
-f.write(userNote+"\n")
+dateToday = str(date.today())
+if not dateSearch(fileName,dateToday):
+    fWriter.write(dateToday + "\n")
+fWriter.write(userNote+"\n")
+fReader = open(fileName, "r")
+notes = fReader.read()
+print(notes)
+
 
 
